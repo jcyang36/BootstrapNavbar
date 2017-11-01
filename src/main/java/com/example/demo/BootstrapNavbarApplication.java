7a1f061131382d1b632d79a8f7ef88f0ad9fbc63
Joyce Yang <jcyang@hmc.edu>
2017-11-01 09:54:41 -0400

    First commit

---
+++ src/main/java/com/example/demo/BootstrapNavbarApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapNavbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapNavbarApplication.class, args);
+	}
+}
